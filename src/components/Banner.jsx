96bf5471756e75011eba327517a850006107b2c7
F e l p <lipjb@hotmail.com>
2023-04-21 01:31:47 -0300

    att: string banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -12,7 +12,7 @@ export const Banner = () => {
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
   const toRotate = ["Developer", "Gamer", "Creator"];
-  const period = 2000;
+  const period = 1000;
 
   useEffect(() => {
     let ticker = setInterval(() => {
@@ -45,7 +45,7 @@ export const Banner = () => {
       setIsDeleting(false);
       setLoopNum(loopNum + 1);
       setIndex(1);
-      setDelta(500);
+      setDelta(200);
     } else {
       setIndex((prevIndex) => prevIndex + 1);
     }
