7a4c4430aa9befa15d17dfe3f56ae1d03aea3fc5
F e l p <lipjb@hotmail.com>
2024-03-22 12:32:04 -0300

    att email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -49,37 +49,38 @@ export const Contact = () => {
 
   // função para envio de email com a lib emailjs
   function sendEmail(e) {
-    e.preventDefault();    
-    
+    e.preventDefault();
+
     // service id e tamplete id
-    
+
     const tampleteparams = {
-      from_name: formDetails.firstName + ' ' +formDetails.lastName,
+      from_name: formDetails.firstName + ' ' + formDetails.lastName,
       message: formDetails.message,
       email: formDetails.email,
       phone: formDetails.phone
     }
-    setButtonText("Sending...");     
+    setButtonText("Sending...");
     // service key e tamplete key do emailJS
     // .then apartir da promisse se a promisse tiver sucesso entra no . then e executa a função anonima 
-    emailjs.send("service_td3prmp", "template_e4bu8ul", tampleteparams, "EC28e_Y3pWDit4HGm")    
-    .then((response)=>{      
-      console.log('EMAIL ENVIADO',response.status,response.text) 
-      setButtonText("Send");
-      if (response.status == 200) {
-        setStatus({ succes: true, message: 'Message sent successfully'});
-      } else {
-        setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
-      }
-       
+    emailjs.send("service_td3prmp", "template_e4bu8ul", tampleteparams, "EC28e_Y3pWDit4HGm")
+      .then((response) => {
+        console.log('EMAIL ENVIADO', response.status, response.text)
+        e.target.reset();
+        setButtonText("Send");
+        if (response.status == 200) {
+          setStatus({ succes: true, message: 'Message sent successfully' });
+        } else {
+          setStatus({ succes: false, message: 'Something went wrong, please try again later.' });
+        }
 
-    }, (error)=>{
-console.log("ERRO:",error)
-    })
-    
+
+      }, (error) => {
+        console.log("ERRO:", error)
+      })
+
   }
- 
-  
+
+
 
   return (
     <section className="contact" id="connect">
@@ -103,16 +104,16 @@ console.log("ERRO:",error)
                         <input type="text" value={formDetails.firstName} placeholder="First Name" autoComplete="off" required onChange={(e) => onFormUpdate('firstName', e.target.value)} />
                       </Col>
                       <Col size={12} sm={6} className="px-1">
-                        <input type="text" value={formDetails.lasttName} placeholder="Last Name"  autoComplete="off" required onChange={(e) => onFormUpdate('lastName', e.target.value)} />
+                        <input type="text" value={formDetails.lasttName} placeholder="Last Name" autoComplete="off" required onChange={(e) => onFormUpdate('lastName', e.target.value)} />
                       </Col>
                       <Col size={12} sm={6} className="px-1">
-                        <input type="email" value={formDetails.email} placeholder="Email Address"  autoComplete="off" required onChange={(e) => onFormUpdate('email', e.target.value)} />
+                        <input type="email" value={formDetails.email} placeholder="Email Address" autoComplete="off" required onChange={(e) => onFormUpdate('email', e.target.value)} />
                       </Col>
                       <Col size={12} sm={6} className="px-1">
-                        <input type="tel" value={formDetails.phone} placeholder="Phone No."  autoComplete="off" required onChange={(e) => onFormUpdate('phone', e.target.value)} />
+                        <input type="tel" value={formDetails.phone} placeholder="Phone No." autoComplete="off" required onChange={(e) => onFormUpdate('phone', e.target.value)} />
                       </Col>
                       <Col size={12} className="px-1">
-                        <textarea rows="6" value={formDetails.message} placeholder="Message"  autoComplete="off" required onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
+                        <textarea rows="6" value={formDetails.message} placeholder="Message" autoComplete="off" required onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
                         <button type="submit"><span>{buttonText}</span></button>
                       </Col>
                       {
