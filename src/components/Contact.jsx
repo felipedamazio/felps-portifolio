5bea919ba53d16bcce8b26c75483ec7a09763192
F e l p <lipjb@hotmail.com>
2024-03-24 21:29:56 -0300

    fix: att

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,76 +1,87 @@
 import { useRef, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 // import { Fullscreen } from "react-bootstrap-icons";
-import emailjs from '@emailjs/browser'
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
+import emailjs from "@emailjs/browser";
+import Swal from "sweetalert2";
 
 // biblioteca de modals prontos
-const MySwal = withReactContent(Swal)
+const mySwal = require(Swal);
 
 export const Contact = () => {
   const formInitialDetails = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    message: ''
-  }
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    message: "",
+  };
 
   const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState('Send');
+  const [buttonText, setButtonText] = useState("Send");
   const [status, setStatus] = useState({});
 
   const onFormUpdate = (category, value) => {
     setFormDetails({
       ...formDetails,
-      [category]: value
-    })
-  }
+      [category]: value,
+    });
+  };
 
   // função para envio de email com a lib emailjs
   function sendEmail(e) {
     e.preventDefault();
     // service id e tamplete id
     const tampleteparams = {
-      from_name: formDetails.firstName + ' ' + formDetails.lastName,
+      from_name: formDetails.firstName + " " + formDetails.lastName,
       message: formDetails.message,
       email: formDetails.email,
-      phone: formDetails.phone
-    }
+      phone: formDetails.phone,
+    };
     setButtonText("Sending...");
     // service key e tamplete key do emailJS
-    // .then apartir da promisse se a promisse tiver sucesso entra no . then e executa a função anonima 
-    emailjs.send("service_td3prmp", "template_e4bu8ul", tampleteparams, "EC28e_Y3pWDit4HGm")
-      .then((response) => {
-        setButtonText("Send");
-        if (response.status == 200) {
-          // setStatus({ succes: true, message: 'Message sent successfully' });
-          console.log('EMAIL ENVIADO', response.status, response.text)
-          Swal.fire({
-            position: "top-end",
-            icon: "success",
-            title: "Email successfully sent !",
-            showConfirmButton: false,
-            timer: 1500
-          });
-          setFormDetails(formInitialDetails)
-        } else {
-          setStatus({ succes: false, message: 'Something went wrong, please try again later.' });
-          Swal.fire({
-            position: "top-end",
-            icon: "error",
-            title: "Sorry, we had trouble sending your email.",
-            showConfirmButton: false,
-            timer: 1500
-          });
+    // .then apartir da promisse se a promisse tiver sucesso entra no . then e executa a função anonima
+    emailjs
+      .send(
+        "service_td3prmp",
+        "template_e4bu8ul",
+        tampleteparams,
+        "EC28e_Y3pWDit4HGm"
+      )
+      .then(
+        (response) => {
+          setButtonText("Send");
+          if (response.status == 200) {
+            // setStatus({ succes: true, message: 'Message sent successfully' });
+            console.log("EMAIL ENVIADO", response.status, response.text);
+            mySwal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "Email successfully sent !",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+            setFormDetails(formInitialDetails);
+          } else {
+            setStatus({
+              succes: false,
+              message: "Something went wrong, please try again later.",
+            });
+            mySwal.fire({
+              position: "top-end",
+              icon: "error",
+              title: "Sorry, we had trouble sending your email.",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+        },
+        (error) => {
+          console.log("ERRO:", error);
         }
-      }, (error) => {
-        console.log("ERRO:", error)
-      })
+      );
   }
   return (
     <section className="contact" id="connect">
@@ -78,41 +89,99 @@ export const Contact = () => {
         <Row className="align-items-center">
           <Col size={12} md={6}>
             <TrackVisibility>
-              {({ isVisible }) =>
-                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us" />
-              }
+              {({ isVisible }) => (
+                <img
+                  className={
+                    isVisible ? "animate__animated animate__zoomIn" : ""
+                  }
+                  src={contactImg}
+                  alt="Contact Us"
+                />
+              )}
             </TrackVisibility>
           </Col>
           <Col size={12} md={6}>
             <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
                   <h2>Talk to Felp</h2>
                   <form onSubmit={sendEmail}>
                     <Row>
                       <Col size={12} sm={6} className="px-1">
-                        <input type="text" value={formDetails.firstName} placeholder="First Name" autoComplete="off" required onChange={(e) => onFormUpdate('firstName', e.target.value)} />
+                        <input
+                          type="text"
+                          value={formDetails.firstName}
+                          placeholder="First Name"
+                          autoComplete="off"
+                          required
+                          onChange={(e) =>
+                            onFormUpdate("firstName", e.target.value)
+                          }
+                        />
                       </Col>
                       <Col size={12} sm={6} className="px-1">
-                        <input type="text" value={formDetails.lastName} placeholder="Last Name" autoComplete="off" required onChange={(e) => onFormUpdate('lastName', e.target.value)} />
+                        <input
+                          type="text"
+                          value={formDetails.lastName}
+                          placeholder="Last Name"
+                          autoComplete="off"
+                          required
+                          onChange={(e) =>
+                            onFormUpdate("lastName", e.target.value)
+                          }
+                        />
                       </Col>
                       <Col size={12} sm={6} className="px-1">
-                        <input type="email" value={formDetails.email} placeholder="Email Address" autoComplete="off" required onChange={(e) => onFormUpdate('email', e.target.value)} />
+                        <input
+                          type="email"
+                          value={formDetails.email}
+                          placeholder="Email Address"
+                          autoComplete="off"
+                          required
+                          onChange={(e) =>
+                            onFormUpdate("email", e.target.value)
+                          }
+                        />
                       </Col>
                       <Col size={12} sm={6} className="px-1">
-                        <input type="tel" value={formDetails.phone} placeholder="Phone No." autoComplete="off" required onChange={(e) => onFormUpdate('phone', e.target.value)} />
+                        <input
+                          type="tel"
+                          value={formDetails.phone}
+                          placeholder="Phone No."
+                          autoComplete="off"
+                          required
+                          onChange={(e) =>
+                            onFormUpdate("phone", e.target.value)
+                          }
+                        />
                       </Col>
                       <Col size={12} className="px-1">
-                        <textarea rows="6" value={formDetails.message} placeholder="Message" autoComplete="off" required onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                        <button type="submit"><span>{buttonText}</span></button>
-                      </Col>                      
+                        <textarea
+                          rows="6"
+                          value={formDetails.message}
+                          placeholder="Message"
+                          autoComplete="off"
+                          required
+                          onChange={(e) =>
+                            onFormUpdate("message", e.target.value)
+                          }
+                        ></textarea>
+                        <button type="submit">
+                          <span>{buttonText}</span>
+                        </button>
+                      </Col>
                     </Row>
                   </form>
-                </div>}
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
     </section>
-  )
-}
+  );
+};
